ca6ce37d28eccc0ec8ebbfc3d3a80b8957ac59c0
Elijah Ndenwa <elijahndenwa19@gmail.com>
2024-06-08 17:42:26 +0100

    implement the dashboard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import Image from "next/image";
 
-const Home = () => {
-  return <div>Home</div>;
-};
+export default function Home() {
+  const loggegIn = { firstName: "John" };
 
-export default Home;
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggegIn?.firstName || "guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
+}
